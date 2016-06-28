0771a79d5d1d10858140f814edae307432ce7a96
PriestofTuna <lyttekk@yahoo.com>
2016-06-27 21:57:19 -0400

    third commit

---
+++ GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by lytte on 6/27/2016.
+ * THINGS
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.print("github pls");
+
+    }
+
+}
